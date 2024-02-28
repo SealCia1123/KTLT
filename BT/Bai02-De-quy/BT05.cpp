c373f1bd741be97911360857a64b5acacc1d5a42
SealCia1123 <maoduchai404@gmail.com>
2024-02-28 13:53:43 +0700

    update BTH2

---
+++ BT/Bai02-De-quy/BT05.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int count(int n);
+
+int main()
+{
+    int n;
+    cout << "Nhap so n: ";
+    cin >> n;
+    cout << "So " << n << " co " << count(n) << " chu so\n";
+    return 0;
+}
+
+int count(int n)
+{
+    if (n < 10)
+        return 1;
+    return 1 + count(n / 10);
+}
---
+++ BT/Bai02-De-quy/BT06.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int myReverse(int n);
+
+int main()
+{
+    int n;
+    cout << "Nhap so nguyen duong n: ";
+    cin >> n;
+    cout << "So dao nguoc cua " << n << " la: " << myReverse(n) << endl;
+    return 0;
+}
+
+int myReverse(int n)
+{
+    int res = 0;
+    if (n < 10)
+        return n;
+    res = myReverse(n / 10);
+    return res;
+}
