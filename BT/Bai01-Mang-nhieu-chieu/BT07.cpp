e85e9b91ce9067a3f486882e1f7b993444ccf003
SealCia1123 <maoduchai404@gmail.com>
2024-03-09 14:52:46 +0700

    update

--- BT/Bai01-Mang-nhieu-chieu/BT07.cpp
+++ BT/Bai01-Mang-nhieu-chieu/BT07.cpp
@@ -10,11 +10,11 @@ void output(const int arr[MAX_ROW][MAX_COL], int row, int col);
 
 double averangeValue(const int arr[MAX_ROW][MAX_COL], int row, int col);
 
-int findValue(const int arr[MAX_ROW][MAX_COL], int row, int col, const int value);
+int findValue(const int arr[MAX_ROW][MAX_COL], int row, int col, int &value);
 
-void swapTwoElements(int arr[MAX_ROW][MAX_COL], int row, int col, int x, int y);
+void swapTwoElements(int arr[MAX_ROW][MAX_COL], int row, int col, int &x, int &y);
 
-int multiply(const int arr[MAX_ROW][MAX_COL], int row, int col, int x);
+int multiply(const int arr[MAX_ROW][MAX_COL], int row, int col, int &x);
 
 int main()
 {
@@ -27,27 +27,21 @@ int main()
     cout << "Gia tri trung binh cua ma tran: " << averangeValue(arr, row, col) << endl;
 
     int value;
-    cout << "Nhap vao gia tri can tim: ";
-    cin >> value;
     if (findValue(arr, row, col, value) == INT_MIN)
         cout << "Khong tim thay " << value << endl;
     else
         cout << "Tim thay " << value << " trong mang\n";
 
     int x, y;
-    cout << "Nhap vao hai gia tri muon hoan doi: ";
-    cin >> x >> y;
     swapTwoElements(arr, row, col, x, y);
     output(arr, row, col);
 
     int mulCol;
-    cout << "Nhap vao cot can tinh tich: ";
-    cin >> mulCol;
-    cout << "Tich cot can tinh: " << multiply(arr, row, col, --mulCol) << endl;
+    cout << "Tich cot can tinh: " << multiply(arr, row, col, mulCol) << endl;
     return 0;
 }
 
-void inputArr(int arr[MAX_ROW][MAX_COL], int row, int col)
+void inputArr(int arr[MAX_ROW][MAX_ROW], int row, int col)
 {
     cout << "Nhap phan tu vao mang: ";
     for (int i = 0; i < row; i++)
@@ -79,8 +73,10 @@ double averangeValue(const int arr[MAX_ROW][MAX_COL], int row, int col)
     return result;
 }
 
-int findValue(const int arr[MAX_ROW][MAX_COL], int row, int col, const int value)
+int findValue(const int arr[MAX_ROW][MAX_COL], int row, int col, int &value)
 {
+    cout << "Nhap vao gia tri can tim: ";
+    cin >> value;
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
@@ -92,33 +88,43 @@ int findValue(const int arr[MAX_ROW][MAX_COL], int row, int col, const int value
     return INT_MIN;
 }
 
-void swapTwoElements(int arr[MAX_ROW][MAX_COL], int row, int col, int x, int y)
+void swapTwoElements(int arr[MAX_ROW][MAX_COL], int row, int col, int &x, int &y)
 {
+    int rX = -1, rY = -1, cX, cY;
+    cout << "Nhap vao hai gia tri muon hoan doi: ";
+    cin >> x >> y;
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
         {
             if (arr[i][j] == x)
             {
-                int temp = arr[i][j];
-                arr[i][j] = findValue(arr, row, col, y);
-
-                for (int k = row - 1; k >= i; --k)
-                {
-                    for (int l = col - 1; l >= 0; --l)
-                    {
-                        if (arr[k][l] == y)
-                            arr[k][l] = temp;
-                        return;
-                    }
-                }
+                rX = i;
+                cX = j;
+            }
+            if (arr[i][j] == y)
+            {
+                rY = i;
+                cY = j;
             }
         }
     }
+
+    if (rX == -1 || rY == -1)
+        cout << "Phan tu khong co trong mang\n";
+    else
+    {
+        int temp = arr[rX][cX];
+        arr[rX][cX] = arr[rY][cY];
+        arr[rY][cY] = temp;
+    }
 }
 
-int multiply(const int arr[MAX_ROW][MAX_COL], int row, int col, int x)
+int multiply(const int arr[MAX_ROW][MAX_COL], int row, int col, int &x)
 {
+    cout << "Nhap vao cot can tinh tich: ";
+    cin >> x;
+    --x;
     int result = 1;
     for (int i = 0; i < row; i++)
         result *= arr[i][x];
