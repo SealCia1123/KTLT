bcaedf728a2581077d576c6b69a9d80fb9c07b8e
SealCia1123 <maoduchai404@gmail.com>
2024-03-10 23:46:01 +0700

    fix error

--- BT/Bai02-De-quy/BT08.cpp
+++ BT/Bai02-De-quy/BT08.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void decToBin(int n);
+int decToBin(int n);
 
 int main()
 {
     int n;
     cout << "Nhap so o he thap phan: ";
     cin >> n;
-    cout << "So o he nhi phan la: ";
-    decToBin(n);
+    cout << "So o he nhi phan la: " << decToBin(n);
+    cout << endl;
     return 0;
 }
 
-void decToBin(int n)
+int decToBin(int n)
 {
-    int temp = n % 2;
     if (n == 0)
-        return;
-    decToBin(n / 2);
-    cout << temp;
+        return 0;
+    else
+    {
+        int temp = n % 2;
+        return decToBin(n / 2) * 10 + temp;
+    }
 }
