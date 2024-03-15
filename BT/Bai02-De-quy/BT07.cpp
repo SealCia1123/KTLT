794dda0650e3fcc70c5ebf2d758f278f6771809e
SealCia1123 <maoduchai404@gmail.com>
2024-03-15 16:19:30 +0700

    update BT07

--- BT/Bai02-De-quy/BT07.cpp
+++ BT/Bai02-De-quy/BT07.cpp
@@ -5,12 +5,23 @@ int sumEvenNum(int n);
 
 int main()
 {
+    int n;
+    cout << "Nhap vao so n: ";
+    cin >> n;
+    cout << "Tong cac chu so chan cua " << n << " la: " << sumEvenNum(n) << endl;
     return 0;
 }
 
 int sumEvenNum(int n)
 {
-    if (n == 0)
+    if (n < 10)
+    {
+        if (n % 2 == 0)
+            return n;
         return 0;
-    return 0;
+    }
+    int temp = n % 10;
+    if (temp % 2 == 0)
+        return temp + sumEvenNum(n / 10);
+    return 0 + sumEvenNum(n / 10);
 }
