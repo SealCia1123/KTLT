e6c07dab63535ed9aaaa106610da39feb2eec864
SealCia1123 <maoduchai404@gmail.com>
2024-03-15 22:29:22 +0700

    delete initialize varible, use directly n to save mem

--- BT/Bai02-De-quy/BT07.cpp
+++ BT/Bai02-De-quy/BT07.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <iostream>
 
 using namespace std;
@@ -18,12 +19,13 @@ int sumEvenNum(int n)
 	if (n < 10)
 	{
 		if (n % 2 == 0)
+		{
 			return n;
+		}
 		return 0;
 	}
 
-	int temp = n % 10;
-	if (temp % 2 == 0)
-		return temp + sumEvenNum(n / 10);
+	if ((n % 10) % 2 == 0)
+		return (n % 10) + sumEvenNum(n / 10);
 	return 0 + sumEvenNum(n / 10);
 }
