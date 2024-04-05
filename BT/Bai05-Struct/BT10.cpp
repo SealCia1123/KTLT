5899112a189b23e606eb04e24cccd3be6335f7c8
SealCia1123 <maoduchai404@gmail.com>
2024-04-05 13:59:27 +0700

    add head file BT10

---
+++ BT/Bai05-Struct/BT10.cpp
@@ -0,0 +1,10 @@
+#include "BT10.h"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	GPG t1, t2;
+	return 0;
+}
---
+++ BT/Bai05-Struct/BT10.h
@@ -0,0 +1,21 @@
+class GPG
+{
+   private:
+	int gio, phut, giay;
+
+   public:
+	int getHour()
+	{
+		return gio;
+	}
+
+	int getMinute()
+	{
+		return phut;
+	}
+
+	int getSecond()
+	{
+		return giay;
+	}
+};
