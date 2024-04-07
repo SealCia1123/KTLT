42736110d27bfb990ca10ac92a5f8f8318fbf11d
SealCia1123 <maoduchai404@gmail.com>
2024-04-07 22:48:33 +0700

    add BTH6

--- BT/Bai06-File-input/BTH6_BT01.cpp
+++ BT/Bai06-File-input/BTH6_BT01.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main()
 {
+	ofstream textFile;
+	textFile.open("BT1.txt", ios::out);
+	int a, b;
+	cout << "Nhap vao hai so nguyen: ";
+	cin >> a >> b;
+	textFile << a + b;
+	textFile.close();
 	return 0;
 }
---
+++ BT/Bai06-File-input/BTH6_BT02.cpp
@@ -0,0 +1,9 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ifstream readFile;
+	return 0;
+}
