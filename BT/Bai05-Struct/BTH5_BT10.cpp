97650cdf97e66e25c5fe8cb02aac0104cafecf43
SealCia1123 <maoduchai404@gmail.com>
2024-04-10 23:38:50 +0700

    update Lab5-BT10

--- BT/Bai05-Struct/BTH5_BT10.cpp
+++ BT/Bai05-Struct/BTH5_BT10.cpp
@@ -6,11 +6,14 @@ using namespace std;
 int main()
 {
 	GPG t1, t2, res;
+	cout << "Nhap vao khoang thoi gian thu nhat:\n";
 	t1.setTime();
 	t1.printGPG();
+	cout << "Nhap vao khoang thoi gian thu hai:\n";
 	t2.setTime();
 	t2.printGPG();
 	res = res.cal(t1, t2);
+	cout << "Khoang cach giua hai khoang thoi gian la: ";
 	res.printGPG();
 	return 0;
 }
--- BT/Bai05-Struct/BTH5_BT10.h
+++ BT/Bai05-Struct/BTH5_BT10.h
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <iostream>
 using namespace std;
 
@@ -9,15 +10,37 @@ class GPG
    public:
 	void setTime()
 	{
+		do
+		{
+			cout << "Nhap vao gio phut giay: ";
+			cin >> gio >> phut >> giay;
+			if ((gio < 1 || gio > 24) || (phut < 1 || phut > 60) || (giay < 1 || giay > 60))
+				cout << "Thoi gian nhap vao khong hop le\n";
+		} while ((gio < 1 || gio > 24) || (phut < 1 || phut > 60) || (giay < 1 || giay > 60));
 	}
 
 	void printGPG()
 	{
+		if (gio < 10)
+			cout << 0 << gio << ":";
+		else
+			cout << gio << ":";
+		if (phut < 10)
+			cout << 0 << phut << ":";
+		else
+			cout << phut << ":";
+		if (giay < 10)
+			cout << 0 << giay << endl;
+		else
+			cout << giay << endl;
 	}
 
-	GPG cal()
+	GPG cal(GPG t1, GPG t2)
 	{
 		GPG res;
+		res.gio = abs(t1.gio - t2.gio);
+		res.phut = abs(t1.phut - t2.phut);
+		res.giay = abs(t1.giay - t2.giay);
 		return res;
 	}
 };
