1f569e2cd9ad1079ab28ac7ed58006667afde4da
SealCia1123 <maoduchai404@gmail.com>
2024-04-19 22:41:47 +0700

    need test on windows bc on not working on linux

--- BT/Bai04-Chuoi/BTH4_BT05.cpp
+++ BT/Bai04-Chuoi/BTH4_BT05.cpp
@@ -1,13 +1,28 @@
-#include <cstring>
 #include <iostream>
+#include <ncurses.h>
 using namespace std;
 
 int main()
 {
+	int i = 0;
 	char password[11];
+	char a;
 	cout << "Nhap mat khau: ";
-	cin >> password;
-	for (int i = 0; i < strlen(password); i++)
-		cout << "X ";
+	while (true)
+	{
+		a = getch();
+		if (a == '\n')
+		{
+			password[i] = '\0';
+			break;
+		}
+		if (a == ' ')
+			continue;
+		cout << "*";
+		password[i++] = a;
+	}
+	cout << endl
+		 << password << endl;
+
 	return 0;
 }
