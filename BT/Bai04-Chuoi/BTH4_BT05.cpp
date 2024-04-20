961822f1a55371226d50c163c2bea7b7403fb3c6
SealCia1123 <maoduchai404@gmail.com>
2024-04-20 08:51:43 +0700

    update Lab4-BT05

--- BT/Bai04-Chuoi/BTH4_BT05.cpp
+++ BT/Bai04-Chuoi/BTH4_BT05.cpp
@@ -1,24 +1,32 @@
-#include <conio.h>
+/* #include <conio.h> */
+#include <cstring>
 #include <iostream>
 using namespace std;
+
 int main()
 {
+	char correctPassword[11] = "SinhVienCQ";
 	char password[11];
 	char temp;
 	int i = 0;
-	while (true)
+	do
 	{
-		temp = getch();
-		if (temp == ' ')
-			continue;
-		if (temp == '\r')
+		while (true)
 		{
-			password[i] = '\0';
-			break;
+			temp = getch();
+			if (temp == ' ')
+				continue;
+			if (temp == '\r')
+			{
+				password[i] = '\0';
+				break;
+			}
+			cout << "X ";
+			password[i++] = temp;
 		}
-		cout << "X ";
-		password[i++] = temp;
-	}
-	cout << endl
-		 << password;
+		if (strcmp(password, correctPassword) != 0)
+			cout << "Nhap sai, nhap lai\n";
+		else
+			cout << "Dang nhap thanh cong\n";
+	} while (strcmp(password, correctPassword) == 0);
 }
