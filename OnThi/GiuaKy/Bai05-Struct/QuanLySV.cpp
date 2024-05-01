bfdb0d5fb617b034e0a7b09a0590ad9f1883e7e0
SealCia1123 <maoduchai404@gmail.com>
2024-05-01 23:42:33 +0700

    update

--- OnThi/GiuaKy/Bai05-Struct/QuanLySV.cpp
+++ OnThi/GiuaKy/Bai05-Struct/QuanLySV.cpp
@@ -23,18 +23,47 @@ void readFile(DanhSachSV &ds);
 
 void print(const DanhSachSV ds);
 
-DanhSachSV sapXepTenGiamDan(const DanhSachSV ds);
+DanhSachSV sortNameDescending(const DanhSachSV ds);
+
+DanhSachSV sortHomeAscending(const DanhSachSV ds);
 
 void swapSV(SinhVien &sv1, SinhVien &sv2);
+
+DanhSachSV searchName(const DanhSachSV ds);
+
+bool datHocBong(const SinhVien sv);
+
+DanhSachSV dsDatHocBong(const DanhSachSV ds);
+
+void xuatDSHocBong(const DanhSachSV ds);
 
 int main()
 {
 	DanhSachSV ds;
 	readFile(ds);
 	print(ds);
-	cout << "================Thu tu ten giam dan===================\n";
-	DanhSachSV dsGiamDan = sapXepTenGiamDan(ds);
-	print(dsGiamDan);
+
+	cout << "\n================Thu tu ten giam dan===================\n";
+	DanhSachSV tenGiamDan = sortNameDescending(ds);
+	print(tenGiamDan);
+
+	cout << "\n================Thu tu que quan tang dan===================\n";
+	DanhSachSV queTangDan = sortHomeAscending(ds);
+	print(queTangDan);
+
+	cout << "\n==================Danh sach sinh vien tim thay===================\n";
+	DanhSachSV timKiem = searchName(ds);
+	print(timKiem);
+
+	cout << "\n======================Danh sach dat hoc bong=========================\n";
+	DanhSachSV hocBong = dsDatHocBong(ds);
+	xuatDSHocBong(hocBong);
+
+	delete[] ds.sv;
+	delete[] tenGiamDan.sv;
+	delete[] queTangDan.sv;
+	delete[] timKiem.sv;
+	delete[] hocBong.sv;
 	return 0;
 }
 
@@ -62,10 +91,10 @@ void readFile(DanhSachSV &ds)
 
 void print(const DanhSachSV ds)
 {
-	cout << "====IN THONG TIN SINH VIEN====\n";
+	cout << "IN THONG TIN SINH VIEN\n";
 	for (int i = 0; i < ds.soLuong; i++)
 	{
-		cout << "====Sinh vien thu " << i + 1 << "====\n";
+		cout << "================================\n";
 		cout << "MSSV: " << ds.sv[i].MSSV << "\n";
 		cout << "Ho ten: " << ds.sv[i].hoTen << "\n";
 		cout << "Que quan: " << ds.sv[i].queQuan << "\n";
@@ -101,11 +130,12 @@ void swapSV(SinhVien &sv1, SinhVien &sv2)
 		sv2.diemMonHoc[i] = temp.diemMonHoc[i];
 }
 
-DanhSachSV sapXepTenGiamDan(const DanhSachSV ds)
+DanhSachSV sortNameDescending(const DanhSachSV ds)
 {
 	DanhSachSV result;
 	result.soLuong = ds.soLuong;
 	result.sv = new SinhVien[result.soLuong];
+
 	for (int i = 0; i < ds.soLuong; i++)
 		result.sv[i] = ds.sv[i];
 
@@ -118,4 +148,85 @@ DanhSachSV sapXepTenGiamDan(const DanhSachSV ds)
 		}
 	}
 	return result;
+}
+
+DanhSachSV sortHomeAscending(const DanhSachSV ds)
+{
+	DanhSachSV result;
+	result.soLuong = ds.soLuong;
+	result.sv = new SinhVien[result.soLuong];
+
+	for (int i = 0; i < ds.soLuong; i++)
+		result.sv[i] = ds.sv[i];
+
+	for (int i = 0; i < result.soLuong - 1; i++)
+	{
+		for (int j = i + 1; j < result.soLuong; j++)
+		{
+			if (strcmp(result.sv[i].queQuan, result.sv[j].queQuan) > 0)
+				swapSV(result.sv[i], result.sv[j]);
+		}
+	}
+	return result;
+}
+
+DanhSachSV searchName(const DanhSachSV ds)
+{
+	string name;
+	cout << "Nhap vao ten can tim: ";
+	getline(cin, name);
+	DanhSachSV result;
+	result.soLuong = 0;
+	result.sv = new SinhVien[ds.soLuong];
+	for (int i = 0; i < ds.soLuong; i++)
+	{
+		if (ds.sv[i].hoTen.find(name) != string::npos)
+			result.sv[(result.soLuong)++] = ds.sv[i];
+	}
+	return result;
+}
+
+bool datHocBong(const SinhVien sv)
+{
+	double diemTong = 0;
+	for (int i = 0; i < SO_MON; i++)
+	{
+		if (sv.diemMonHoc[i] < 5)
+			return false;
+		diemTong += sv.diemMonHoc[i];
+	}
+	return (diemTong / SO_MON > 7.5) ? true : false;
+}
+
+DanhSachSV dsDatHocBong(const DanhSachSV ds)
+{
+	DanhSachSV result;
+	result.sv = new SinhVien[ds.soLuong];
+	result.soLuong = 0;
+
+	for (int i = 0; i < ds.soLuong; i++)
+	{
+		if (datHocBong(ds.sv[i]) == true)
+			result.sv[(result.soLuong)++] = ds.sv[i];
+	}
+	return result;
+}
+
+void xuatDSHocBong(const DanhSachSV ds)
+{
+	ofstream f;
+	f.open("hocbong.txt", ios::out);
+	f << "Danh sach dat hoc bong" << endl;
+	for (int i = 0; i < ds.soLuong; i++)
+	{
+		f << "\n=====Sinh vien thu " << i + 1 << "=====" << endl;
+		f << "MSSV: " << ds.sv[i].MSSV << endl;
+		f << "Ho ten: " << ds.sv[i].hoTen << endl;
+		f << "Que quan: " << ds.sv[i].queQuan << endl;
+		f << "Diem:\t";
+		for (int j = 0; j < SO_MON; j++)
+			f << ds.sv[i].diemMonHoc[j] << "\t";
+		f << endl;
+	}
+	f.close();
 }
