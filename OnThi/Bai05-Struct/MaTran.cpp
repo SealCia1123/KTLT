1284e906f581295d51eeddb5d2ffca1a3c12d08a
SealCia1123 <maoduchai404@gmail.com>
2024-05-06 23:24:15 +0700

    update

--- OnThi/Bai05-Struct/MaTran.cpp
+++ OnThi/Bai05-Struct/MaTran.cpp
@@ -68,34 +68,58 @@ class Matrix
 	Matrix mulMatrix(const Matrix mt2)
 	{
 		// a[n][m] x b[p][q] = c[n][q]
-		Matrix mt3;
+		Matrix mulMT;
 		if (this->col != mt2.row)
 		{
-			mt3.row = -1;
-			mt3.col = -1;
-			return mt3;
+			mulMT.row = -1;
+			mulMT.col = -1;
+			return mulMT;
 		}
-		mt3.row = this->row;
-		mt3.col = mt2.col;
-		mt3.mt = new int *[mt3.row];
-		for (int i = 0; i < mt3.row; i++)
-			mt3.mt[i] = new int[mt3.col];
+		mulMT.row = this->row;
+		mulMT.col = mt2.col;
+		mulMT.mt = new int *[mulMT.row];
+		for (int i = 0; i < mulMT.row; i++)
+			mulMT.mt[i] = new int[mulMT.col];
 
-		for (int i = 0; i < mt3.row; i++)
+		for (int i = 0; i < mulMT.row; i++)
 		{
-			for (int j = 0; j < mt3.col; j++)
+			for (int j = 0; j < mulMT.col; j++)
 			{
 				for (int k = 0; k < this->col; k++)
-					mt3.mt[i][j] += this->mt[i][k] + mt2.mt[k][j];
+					mulMT.mt[i][j] += this->mt[i][k] + mt2.mt[k][j];
 			}
 		}
-		return mt3;
+		return mulMT;
+	}
+
+	Matrix sumMatrix(const Matrix mt2)
+	{
+		// a[n][m] + b[n][m] =  c[n][m]
+		Matrix sumMT;
+		if (this->row != mt2.row || this->col != mt2.col)
+		{
+			sumMT.row = -1;
+			sumMT.col = -1;
+			return sumMT;
+		}
+		sumMT.row = this->row;
+		sumMT.col = this->col;
+		sumMT.mt = new int *[sumMT.row];
+		for (int i = 0; i < sumMT.row; i++)
+			sumMT.mt[i] = new int[sumMT.col];
+
+		for (int i = 0; i < sumMT.row; i++)
+		{
+			for (int j = 0; j < sumMT.col; j++)
+				sumMT.mt[i][j] = this->mt[i][j] + mt2.mt[i][j];
+		}
+		return sumMT;
 	}
 };
 
 int main()
 {
-	Matrix mt1, mt2, mt3;
+	Matrix mt1, mt2, mulMT, sumMT;
 	mt1.initMatrix();
 	cout << "Matrix 1:\n";
 	mt1.print();
@@ -104,12 +128,27 @@ int main()
 	cout << "Matrix 2:\n";
 	mt2.print();
 
-	mt3 = mt1.mulMatrix(mt2);
-	cout << "Matrix multiply:\n";
-	mt3.print();
+	mulMT = mt1.mulMatrix(mt2);
+	if (mulMT.getRow() == -1 || mulMT.getCol() == -1)
+		cout << "Can't multiply two matrix with different size!\n";
+	else
+	{
+		cout << "Multiply matrix:\n";
+		mulMT.print();
+		mulMT.freeMt();
+	}
+
+	sumMT = mt1.sumMatrix(mt2);
+	if (sumMT.getRow() == -1 || sumMT.getCol() == -1)
+		cout << "Can't multiply two matrix with different size!\n";
+	else
+	{
+		cout << "Sum matrix:\n";
+		sumMT.print();
+		sumMT.freeMt();
+	}
 
 	mt1.freeMt();
 	mt2.freeMt();
-	mt3.freeMt();
 	return 0;
 }
---
+++ OnThi/Bai05-Struct/PhanSo.cpp
@@ -0,0 +1,159 @@
+/*
+
+De bai:
+Cho ma tran voi moi phan tu la mot phan so
+a) Nhap va xuat ma tran voi dang tu/mau
+b) Toi gian phan so
+c) Tinh tong cac phan tu cua ma tran
+d) Tim phan so lon nhat cua ma tran
+e) Tim va in ra phan so lon nhat tren tung dong
+
+*/
+
+#include <iomanip>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int UCLN(int a, int b)
+{
+	a = abs(a);
+	b = abs(b);
+	if (b == 0)
+		return a;
+	return UCLN(b, a % b);
+}
+
+struct PhanSo
+{
+	int tuSo, mauSo;
+
+	void dinhDang()
+	{
+		if (tuSo == mauSo)
+		{
+			tuSo = 1;
+			mauSo = 1;
+		}
+		if (mauSo < 0)
+		{
+			tuSo *= -1;
+			mauSo *= -1;
+		}
+	}
+
+	void toiGian()
+	{
+		int temp = tuSo;
+		tuSo /= UCLN(tuSo, mauSo);
+		mauSo /= UCLN(temp, mauSo);
+	}
+
+	void nhapPS()
+	{
+		int tuSo, mauSo;
+		do
+		{
+			cout << "Nhap vao tu so va mau so: ";
+			cin >> tuSo >> mauSo;
+			if (mauSo == 0)
+				cout << "Loi chia cho 0\n";
+		} while (mauSo == 0);
+		this->tuSo = tuSo;
+		this->mauSo = mauSo;
+		dinhDang();
+	}
+
+	void inPS()
+	{
+		toiGian();
+		dinhDang();
+		if (mauSo == 1)
+			cout << tuSo;
+		else if (tuSo == 0)
+			cout << 0;
+		else
+			cout << tuSo << "/" << mauSo;
+	}
+
+	int soSanh(PhanSo p)
+	{
+		float temp1 = (float)this->tuSo / this->mauSo;
+		float temp2 = (float)p.tuSo / p.mauSo;
+		if (temp1 == temp2)
+			return 0;
+		else if (temp1 > temp2)
+			return 1;
+		return -1;
+	}
+};
+
+struct MaTranPS
+{
+	int row, col;
+	PhanSo **arr;
+
+	void nhapMT()
+	{
+		int r, c;
+		cout << "Nhap so dong va so cot: ";
+		cin >> r >> c;
+		row = r;
+		col = c;
+
+		arr = new PhanSo *[row];
+		for (int i = 0; i < row; i++)
+			arr[i] = new PhanSo[col];
+
+		cout << "\nBat dau nhap vao ma tran phan so\n";
+		for (int i = 0; i < row; i++)
+		{
+			for (int j = 0; j < col; j++)
+				arr[i][j].nhapPS();
+		}
+	}
+
+	void inMT()
+	{
+		cout << "Ma tran:\n";
+		for (int i = 0; i < row; i++)
+		{
+			for (int j = 0; j < col; j++)
+			{
+				arr[i][j].inPS();
+				cout << "\t";
+			}
+			cout << "\n";
+		}
+	}
+
+	void freeMT()
+	{
+		for (int i = 0; i < row; i++)
+			delete[] arr[i];
+		delete[] arr;
+	}
+};
+
+double tongMaTran(const MaTranPS mt)
+{
+	double sum = 0;
+	for (int i = 0; i < mt.row; i++)
+	{
+		for (int j = 0; j < mt.col; j++)
+			sum += (double)mt.arr[i][j].tuSo / mt.arr[i][j].mauSo;
+	}
+	return sum;
+}
+
+int main()
+{
+	MaTranPS p;
+	p.nhapMT();
+	p.inMT();
+	double t = tongMaTran(p);
+	cout << "Tong cac phan tu trong ma tran: " << fixed << setprecision(2) << t << "\n";
+
+	p.freeMT();
+	return 0;
+}
