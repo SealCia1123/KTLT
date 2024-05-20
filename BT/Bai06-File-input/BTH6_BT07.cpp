1b3899e0e44ee7922a571441fc237e06b5cd65bc
SealCia1123 <maoduchai404@gmail.com>
2024-05-20 10:37:32 +0700

    update

--- BT/Bai06-File-input/BTH6_BT07.cpp
+++ BT/Bai06-File-input/BTH6_BT07.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 int main()
 {
+	ofstream outData;
+	outData.open("test.txt", ios::out);
+	while (true)
+	{
+		int temp;
+		cin >> temp;
+		if (temp == -1)
+			break;
+		outData << temp << " ";
+	}
+	outData.close();
 	return 0;
 }
